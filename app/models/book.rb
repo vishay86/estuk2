93c30bf04d2028c4c80f7fe55448f567a744c069
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-02-15 09:53:47 +0400

    Images now showing dynamically

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ActiveRecord::Base
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, message: 'Only images allowed'
 
   has_attached_file :resource
+  validates_attachment_content_type :resource, content_type: ['application/pdf'], message: 'Only PDF allowed'
 
   validates_presence_of :name, :author, :description, :price
 
