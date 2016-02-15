7de3ac043e5f95d6fa8180a1b0d2bc82cbf61f7c
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-02-15 12:50:31 +0400

    Added sales model, validated price numericality

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
 
 	extend FriendlyId
 	friendly_id :name, use: :slugged
-	
+
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, message: 'Only images allowed'
 
@@ -11,6 +11,10 @@ class Book < ActiveRecord::Base
 
   validates_presence_of :name, :author, :description, :price
 
+  validates_numericality_of :price,
+  greater_than: 49, message: 'Must be greater than 50 cents' 
+
 
 	belongs_to :user
+	has_many :sales
 end
